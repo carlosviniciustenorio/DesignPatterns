27eb8836a019aef35f6210603ba8f97d397b255a
Carlos Vinícius <47374132+carlosviniciustenorio@users.noreply.github.com>
2022-10-11 08:40:59 -0300

    abstract factory

--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -1,128 +1,56 @@
 ﻿public class Program
 {
-    public void Main(string[] args)
+    public static void Main()
     {
-        // Abstract factory #1
-            AbstractFactory factory1 = new ConcreteFactory1();
-            Client client1 = new Client(factory1);
-            client1.Run();
-            // Abstract factory #2
-            AbstractFactory factory2 = new ConcreteFactory2();
-            Client client2 = new Client(factory2);
-            client2.Run();
-            // Wait for user input
-            Console.ReadKey(); 
+        var navigationApple = new NavigationBar(new Apple());
+        var dropDownApple = new DropDownMenu(new Apple());
+
+        Console.WriteLine(@$"It is a {navigationApple.GetType().Name}");
+        Console.WriteLine(@$"It is a {dropDownApple.GetType().Name}");
+
+        var navigationAndroid = new NavigationBar(new Android());
+        var dropdownAndroid = new DropDownMenu(new Android());
+        Console.WriteLine(@$"/r");
+        Console.WriteLine(@$"It is a {navigationAndroid.GetType().Name}");
+        Console.WriteLine(@$"It is a {dropdownAndroid.GetType().Name}");
     }
 
-    /// <summary>
-    /// The 'AbstractFactory' abstract class
-    /// </summary>
-    abstract class AbstractFactory
+    public class NavigationBar
     {
-        public abstract AbstractProductA CreateProductA();
-        public abstract AbstractProductB CreateProductB();
+        public NavigationBar(UIFactory factory) => factory.CreateButton();
     }
 
-    /// <summary>
-    /// The 'ConcreteFactory1' class
-    /// </summary>
-    class ConcreteFactory1 : AbstractFactory
+    public class DropDownMenu
     {
-        public override AbstractProductA CreateProductA()
+        public DropDownMenu(UIFactory factory) => factory.CreateButton();
+    }
+
+    public interface UIFactory
+    {
+        public Button CreateButton();
+    }
+
+    public class Apple : UIFactory
+    {
+        public Button CreateButton()
         {
-            return new ProductA1();
-        }
-        public override AbstractProductB CreateProductB()
-        {
-            return new ProductB1();
-        }
-    }
-    
-    /// <summary>
-    /// The 'ConcreteFactory2' class
-    /// </summary>
-    class ConcreteFactory2 : AbstractFactory
-    {
-        public override AbstractProductA CreateProductA()
-        {
-            return new ProductA2();
-        }
-        public override AbstractProductB CreateProductB()
-        {
-            return new ProductB2();
-        }
-    }
-    
-    /// <summary>
-    /// The 'AbstractProductA' abstract class
-    /// </summary>
-    abstract class AbstractProductA
-    {
-    }
-    
-    /// <summary>
-    /// The 'AbstractProductB' abstract class
-    /// </summary>
-    abstract class AbstractProductB
-    {
-        public abstract void Interact(AbstractProductA a);
-    }
-    
-    /// <summary>
-    /// The 'ProductA1' class
-    /// </summary>
-    class ProductA1 : AbstractProductA
-    {
-    }
-    
-    /// <summary>
-    /// The 'ProductB1' class
-    /// </summary>
-    class ProductB1 : AbstractProductB
-    {
-        public override void Interact(AbstractProductA a)
-        {
-            Console.WriteLine(this.GetType().Name +
-              " interacts with " + a.GetType().Name);
-        }
-    }
-    
-    /// <summary>
-    /// The 'ProductA2' class
-    /// </summary>
-    class ProductA2 : AbstractProductA
-    {
-    }
-    
-    /// <summary>
-    /// The 'ProductB2' class
-    /// </summary>
-    class ProductB2 : AbstractProductB
-    {
-        public override void Interact(AbstractProductA a)
-        {
-            Console.WriteLine(this.GetType().Name +
-              " interacts with " + a.GetType().Name);
+            return new Button { Type = "iOS Button" };
         }
     }
 
-    /// <summary>
-    /// The 'Client' class. Interaction environment for the products.
-    /// </summary>
-    class Client
+    public class Android : UIFactory
     {
-        private AbstractProductA _abstractProductA;
-        private AbstractProductB _abstractProductB;
-        // Constructor
-        public Client(AbstractFactory factory)
+        public Button CreateButton()
         {
-            _abstractProductB = factory.CreateProductB();
-            _abstractProductA = factory.CreateProductA();
-        }
-        public void Run()
-        {
-            _abstractProductB.Interact(_abstractProductA);
+            return new Button { Type = "Android Button" };
         }
     }
 
+    public class Button{
+        public string Type { get; set; }
+        public Button()
+        {
+            
+        }
+    }
 }
